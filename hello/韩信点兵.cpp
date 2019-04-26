9a2ed9c400c49a12ddbed0c6d5df3fbca1a621dd
Qlin00 <1353064275@qq.com>
2019-04-26 13:27:47 +0800

    hanxin

--- hello/韩信点兵.cpp
+++ hello/韩信点兵.cpp
@@ -7,24 +7,34 @@ class HanXin{
 		int no7;
 		int many;
 	public:
-		HanXin() {
+		HanXin() ;
+		~HanXin(){
+			cout<<"hanxin die" << endl;
+		}
+		void line3(int n1);
+		void line5(int n2);
+		void line7(int n3);
+		void howMany();
+		void showMany();
+};
+HanXin::HanXin() {
 			no3 = 0;
 			no5 = 0;
 			no7 = 0;
 			many = 0;
 		}
-		void line3(int n1){
+void HanXin :: line3(int n1){
 			no3 = n1;
-		}
-		void line5(int n2){
+	    }
+void HanXin :: line5(int n2){
 			no5 = n2;
 		}	
-		void line7(int n3){
+void HanXin :: line7(int n3){
 			no7 = n3;
 		}
-		void howMany(){
+void HanXin :: howMany(){
 			int i = 10;
-			for(i; i <= 100; i++){
+			for(i; i <= 100; i++){	
 				if(i % 3 == no3 && i % 5 == no5 && i % 7 == no7)
 					break;
 			}
@@ -32,15 +42,14 @@ class HanXin{
 				many = i;
 			else
 				many = 0;
-		}
-		void showMany(){
+}
+void HanXin ::  showMany(){
 			howMany();
 			if(many == 0)
 				std::cout << "Impossible" << endl;
 			else
 				std::cout << many << endl;
 		}
-};
 int main() {
     int n, n1, n2, n3;
     std::cin >> n;
